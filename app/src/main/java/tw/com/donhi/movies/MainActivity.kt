aa7bfab9910f8e65033bf7dad100433ee420bc47
Tetsu <ttgchang@gmail.com>
2024-09-18 14:59:15 +0800

    取得電影JSON資料

---
+++ app/src/main/java/tw/com/donhi/movies/MainActivity.kt
@@ -0,0 +1,67 @@
+package tw.com.donhi.movies
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import tw.com.donhi.movies.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    val TAG = MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val client = OkHttpClient()
+        CoroutineScope(Dispatchers.IO).launch {
+            val request = Request.Builder()
+                .url("https://api.themoviedb.org/3/movie/popular?language=zh-TW&page=1")
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDU0MzM3MWUwZjA0OWIzMzg2Njg4OGMxZDQxYWE5YyIsIm5iZiI6MTcyNjY0MDk2Mi42MDk4OTEsInN1YiI6IjY2ZWE3MWQxYjI5MTdlYjE4MDBiNTAwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EEB7Xuqy3GXRfxQ71XFEPXnk3TvWLyxWcRkQ6Ks226Y")
+                .build()
+
+            val response = client.newCall(request).execute()
+            Log.d(TAG, "onCreate: ${response.body?.string()}")
+        }
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .setAnchorView(R.id.fab).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+}
